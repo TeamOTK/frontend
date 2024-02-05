d5d79ccb42ddb92ccedf9a8df14764451b487f84
hsh <hsh200315@naver.com>
2024-02-05 15:25:03 +0900

    feat: 캐릭터 페이지 Switch 구현 및 적용

--- src/components/CharacterPage.jsx
+++ src/components/CharacterPage.jsx
@@ -1,11 +1,16 @@
 import { useNavigate } from 'react-router-dom';
+import { useState  } from 'react';
 
 import './Pages.css'
-import CM from './images/CM.png'
+
+import SwitchComponent from './Switch';
 import { BsSearch } from "react-icons/bs";
+import CharacterFicion from './Character_Fiction_Page';
+import CharacterOriginal from './Character_Original_Page';
 
 
 export default function CharacterPage(){
+	const [isOn, setisOn] = useState(false);
 	const navigate = useNavigate();
 
 	const handleClickSearch= () => {
@@ -18,21 +23,15 @@ export default function CharacterPage(){
 
 	return(
 		<div className='MainPageBackGround'>
+			<div className='CharacterHeader'>
+				<SwitchComponent isOn = {isOn} setisOn={setisOn}/>
+				<BsSearch size={30} style={{marginRight:'3%',fontWeight:'bold'}} onClick={handleClickSearch}/>
+			</div>
 			<div className='CharacterLargeTextSet'>
-				<h2 className='MainPageLargeText'>웹툰 등장 인물과 대화해보세요!</h2>
-				<BsSearch size={30} style={{marginLeft:'1%'}} onClick={handleClickSearch}/>
+				<h2 className='CharacterPageLargeText'>웹툰 등장 인물과 대화해보세요!</h2>
 			</div>
-			<div className='CharacterList'>
-				{/* map 함수 이용해 아이템 추가 */}
-				<div className='CharacterItem' onClick={handleClickCharacter}>
-					<img src={CM} className='CharacterImg'/>
-					<div className='CharacterDesc'>
-						<h3 style={{fontWeight:"bold"}}>청명</h3>
-						<div>네이버웹툰 화산귀환</div>
-						<h2 style={{fontWeight:"bold"}}>"쫄리면 뒈지시던가"</h2>
-					</div>
-				</div>
-			</div>
+			{isOn === true ? <CharacterFicion handleClickCharacter={handleClickCharacter}/> :  <CharacterOriginal handleClickCharacter={handleClickCharacter}/>}
+			
 		</div>
 	)
 }
